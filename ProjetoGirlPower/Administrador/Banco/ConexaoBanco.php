084b07894df05046cd4787e831c6b3bf0bbd74e5
BiiaSouzaCardoso <77284043+BiiaSouzaCardoso@users.noreply.github.com>
2021-01-11 15:29:47 -0300

    Delete ConexaoBanco.php

--- ProjetoGirlPower/Administrador/Banco/ConexaoBanco.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $Conexao_Banco = mysqli_connect("localhost", "RicDB", "Ricardo@220820", "GirlPower");
-    if(!$Conexao_Banco){
-        echo "Deu erro ao se conectar ao banco";
-    }
-?>
